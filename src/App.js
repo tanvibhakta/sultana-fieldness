5d2593f0b78e12a9e5424ceb38cf94d3cda73226
Tanvi Bhakta <tanvibhakta@gmail.com>
2021-11-01 15:15:37 +0530

    Record audio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import {Recorder} from 'react-voice-recorder';
+import 'react-voice-recorder/dist/index.css';
+
 import './App.css';
 
+
 function App() {
+
+    const [audioDetails, setAudioDetails] = useState({
+        url: null,
+        blob: null,
+        chunks: null,
+        duration: {h: null, m: null, s: null}
+    })
+
+    const handleAudioStop = (data) => {
+        console.log(data);
+        setAudioDetails(data);
+    }
+
+    const handleAudioUpload = (file) => {
+        console.log(file);
+    }
+
+    const handleReset= () => {
+        const reset = {
+            url: null,
+            blob: null,
+            chunks: null,
+            duration: {h: null, m: null, s: null,}
+        }
+        setAudioDetails(reset);
+    }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Here is an audio experiment</h2>
+      <h4>Start Recording</h4>
+      <h4>Stop Recording </h4>
+      <h4>Play</h4>
+      <h4>Pause</h4>
+
+        <Recorder
+            record={true}
+            title={"New recording"}
+            audioURL={audioDetails.url}
+            showUIAudio
+            handleAudioStop={data => handleAudioStop(data)}
+            // handleOnChange={(value) => handleOnChange(value, 'firstname')}
+            handleAudioUpload={data => handleAudioUpload(data)}
+            handleReset={() => handleReset()} />
     </div>
   );
 }
