a70518c5c39e6a42d87796e1e7699f622fe06410
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-01-14 21:52:05 +0530

    Add svg bubbles

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 import Modal from "react-modal";
+import { ReactComponent as BubbleA } from "../assets/bubbles/bubble-a.svg";
+import { ReactComponent as BubbleB } from "../assets/bubbles/bubble-b.svg";
+import { ReactComponent as BubbleC } from "../assets/bubbles/bubble-c.svg";
+import { ReactComponent as BubbleD } from "../assets/bubbles/bubble-d.svg";
+import { ReactComponent as BubbleE } from "../assets/bubbles/bubble-e.svg";
+import { ReactComponent as BubbleF } from "../assets/bubbles/bubble-f.svg";
+import { ReactComponent as BubbleG } from "../assets/bubbles/bubble-g.svg";
 
-export const Bubble = () => {
+export const Bubble = ({ className }) => {
   const [showModal, setShowModal] = useState(false);
   return (
     <div>
-      <button
+      <BubbleA
         onClick={() => {
           setShowModal(true);
         }}
+        className={`${className}`}
       >
         bub
-      </button>
+      </BubbleA>
       <div className={`bubble`}>
         <Modal
           isOpen={showModal}
