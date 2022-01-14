bcce74228e85f4a12746577edb08339e16f4807d
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-01-14 17:37:31 +0530

    Remove extra stuff

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Page1 } from "./pages/Page1";
 import { Questions } from "./components/Questions";
 import { BubbleInput } from "./pages/BubbleInput";
@@ -31,8 +30,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
