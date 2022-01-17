a3015b58936f27c50a2016886c3fec02a7502229
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-01-17 12:08:15 +0530

    Make bubble modal

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -9,6 +9,9 @@ import { ReactComponent as BubbleD } from "../assets/bubbles/bubble-d.svg";
 import { ReactComponent as BubbleE } from "../assets/bubbles/bubble-e.svg";
 import { ReactComponent as BubbleF } from "../assets/bubbles/bubble-f.svg";
 import { ReactComponent as BubbleG } from "../assets/bubbles/bubble-g.svg";
+import { ReactComponent as AudioIcon } from "../assets/audio.svg";
+import { ReactComponent as CollectionJar } from "../assets/collection-jar.svg";
+import { ReactComponent as ShareIcon } from "../assets/share.svg";
 
 // TODO: reclassify bubble types from alphabet based system to purpose based system
 // TODO: switch case that renders correct svg given type
@@ -31,11 +34,29 @@ export const Bubble = ({ className }) => {
           onRequestClose={() => {
             setShowModal(false);
           }}
+          className={"modal green-background"}
+          overlayClassName={"overlay"}
         >
-          <div>geolocation</div>
-          <div>creation timestamp</div>
-          <div>country/city</div>
-          <div>text content if any</div>
+          <div className="bubble-container">
+            <div className="user-id-and-audio">
+              <div>{"<user id>"}</div>
+              <AudioIcon></AudioIcon>
+            </div>
+            <div className="description">
+              this is the description of the sounds curated from different
+              places and times. This is the most important part. This is what
+              will be transformative.
+            </div>
+            <div className="meta">
+              <span>geoloc</span>
+              <span>time</span>
+              <span>country/city</span>
+            </div>
+            <div className="collect-and-share">
+              <CollectionJar></CollectionJar>
+              <ShareIcon></ShareIcon>
+            </div>
+          </div>
         </Modal>
       </div>
     </div>
