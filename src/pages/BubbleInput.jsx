9cdc8085bbe552d3195cd353a93fc30a471c275b
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-01-21 21:18:53 +0530

    Style the bubble input page

--- src/pages/BubbleInput.jsx
+++ src/pages/BubbleInput.jsx
@@ -1,17 +1,34 @@
 import { Link } from "react-router-dom";
+import "./css/bubbleinput.css";
 
 export const BubbleInput = () => {
   // TODO: Ask for permissions for location, audio recording, video recording APIs here
   return (
-    <div>
+    <div className="bubble_input-container container">
+      <p>You are now creating a seed</p>
       <form>
         {/*TODO: Put a WISYWIG here, perhaps TinyMCE?*/}
-        <input type="textarea" />
+        <div className="bubble_input-description">
+          <label
+            htmlFor="description"
+            className="bubble_input-description-label"
+          >
+            Input Text
+          </label>
+          <input
+            name="description"
+            id="description"
+            className="bubble_input-description-input"
+            type="textarea"
+          />
+        </div>
         <button>Record Audio</button>
         <button>Click picture</button>
-        <button type="submit">Skip</button>
+        {/*<button type="submit">Skip</button>*/}
         <Link to="/created">
-          <button type="submit">submit</button>
+          <button className="bubble_input-submit" type="submit">
+            Save>>
+          </button>
         </Link>
       </form>
     </div>
