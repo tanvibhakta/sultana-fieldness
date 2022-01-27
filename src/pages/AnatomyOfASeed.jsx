bfd1ca4bdea4437423c2c475f89a4cf785c0723f
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-01-27 11:18:33 +0530

    Add assets, text to anatomy of a seed page

--- src/pages/AnatomyOfASeed.jsx
+++ src/pages/AnatomyOfASeed.jsx
@@ -1,5 +1,26 @@
 import "./css/info.css";
+import anatomy from "../assets/anatomy-of-a-seed.png";
+import anatomy2x from "../assets/anatomy-of-a-seed@2x.png";
+import anatomy3x from "../assets/anatomy-of-a-seed@3x.png";
 
 export const AnatomyOfASeed = () => (
-  <div className="anatomy-container">Anatomy of a Seed</div>
+  <div className="anatomy-container">
+    <div>
+      <img
+        srcSet={`${anatomy}, ${anatomy2x} 2x, ${anatomy3x} 3x`}
+        src={anatomy}
+      />
+      <figcaption>Anatomy of a Seed</figcaption>
+    </div>
+    <div>
+      <p>
+        Seeds are events or encounters with more than human fellow beings-
+        either as sonic impressions of their presence or absense.
+      </p>
+      <p>
+        A seed is an observation : a snapshot of ones entanglement with other
+        beings.
+      </p>
+    </div>
+  </div>
 );
