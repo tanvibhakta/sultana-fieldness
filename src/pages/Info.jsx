7ff31700c28f9bb3a21f163d11e688bd51d89b8a
SULTANAZANA <36982021+SULTANAZANA@users.noreply.github.com>
2022-08-28 15:25:56 +0530

    typos update

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -5,7 +5,7 @@ export const Info = () => {
   return (
     <div className="info-container default-page-spacing">
       <div className="information">
-        <p>Welcome to Fieldness?</p>
+        <p>Welcome to Fieldness</p>
         <p>
           Fieldness is a kind of audio diary. Each entry is a seed. You can create your own collection over time and space. 
 
@@ -20,7 +20,7 @@ export const Info = () => {
 Fieldness started with the need to create a tool for myself to practice listening. To be able to sense the world through sound, being able to recall and revisit these sounds. Seeds became a metaphor for ‘a sonic observation’ bound by time and space. And Fieldness is the idea to interconnect our fields with others. 
 
         </p>
-        <p>You are invited you to listen to fellow non human
+        <p>You are invited to listen to fellow non human
           beings and share these encounters as seeds on the platform.
            <br /> Conceptualized and designed by Sultana Zana. 
 
@@ -28,5 +28,9 @@ Fieldness started with the need to create a tool for myself to practice listenin
       </div>
       <Link to="/anatomy-of-a-seed">Anatomy of a seed >></Link>
     </div>
+    </div>
+      <Link to="">More >></Link>
+    </div>
+    
   );
 };
