e7bb4985927b789d7fd558c61b08cb041846a131
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-09-13 12:07:25 +0530

    Landing page now displays all kaldi moss seeds

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,53 @@
 import { Link } from "react-router-dom";
 import { Bubble } from "../components/Bubble";
 import "./css/landingpage.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../lib/UserContext";
 import { ReactComponent as CollectionJar } from "../assets/collection-jar.svg";
 import { ReactComponent as EmptySeed } from "../assets/empty_seed.svg";
 import { ReactComponent as Arrow } from "../assets/arrow.svg";
+import { getUser } from "../api";
 
 export const LandingPage = () => {
   const user = useContext(UserContext).user;
+  const [landing, setLanding] = useState(null);
+
+  useEffect(() => {
+    // Async here instead of outer function to prevent race conditions
+    async function fetchData() {
+      await getUser("kaldi moss")
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          if (JSON.stringify(landing) != JSON.stringify(data)) {
+            setLanding(data);
+          }
+        })
+        .catch((response) => {
+          console.error(response.status);
+          setLanding(landing);
+        });
+    }
+    fetchData();
+  });
+
+  const columns = { A: [], B: [], C: [], D: [] };
+  landing?.seedCreated.map((seedID, i) => {
+    if (i < 3) {
+      columns.A.push(seedID);
+    } else if (i < 9) {
+      columns.B.push(seedID);
+    } else if (i < 13) {
+      columns.C.push(seedID);
+    } else if (i < 17) {
+      columns.D.push(seedID);
+    } else {
+      //  TODO: What if the number of seeds increases 17?
+    }
+  });
 
   return (
     <div className="container landing-page-container">
@@ -33,28 +72,24 @@ export const LandingPage = () => {
       </div>
       <div className="bubbles-container">
         <div className="column">
-          <Bubble className="bubble_a" />
-          <Bubble />
-          <Bubble />
+          {columns.A.map((seedID) => (
+            <Bubble key={seedID} id={seedID} name="kaldi moss" />
+          ))}
         </div>
         <div className="column">
-          <Bubble />
-          <Bubble />
-          <Bubble />
-          <Bubble />
-          <Bubble />
-          <Bubble />
-          <Bubble />
+          {columns.B.map((seedID) => (
+            <Bubble key={seedID} id={seedID} name="kaldi moss" />
+          ))}
         </div>
         <div className="column">
-          <Bubble />
-          <Bubble />
-          <Bubble />
-          <Bubble />
+          {columns.C.map((seedID) => (
+            <Bubble key={seedID} id={seedID} name="kaldi moss" />
+          ))}
         </div>
         <div className="column">
-          <Bubble />
-          <Bubble />
+          {columns.D.map((seedID) => (
+            <Bubble key={seedID} id={seedID} name="kaldi moss" />
+          ))}
         </div>
       </div>
     </div>
